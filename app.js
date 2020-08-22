9719d614dc4f432c819b6864f3de83025c78a9af
unknown <elormavugal7@gmail.com>
2020-08-22 21:47:46 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const flash = require("connect-flash");
+const session = require("express-session");
+
+
+const app = express();
+
+// passport config
+require("./config/passport")(passport);
+
+//DB Config
+const db = require("./config/keys").mongoURI;
+
+//connect to MongoDB
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log(err));
+
+//EJS
+app.use(expressLayouts); //make sure the layout is about otherwise it will not work
+app.set("view engine", "ejs");
+
+//Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+//Express session
+app.use(
+    session({
+        secret: "secret",
+        resave: true,
+        saveUninitialized: true,
+    }),
+);
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//connect flash
+app.use(flash());
+
+//Global variables
+app.use(function (req, res, next) {
+    res.locals.success_msg = req.flash("success_msg");
+    res.locals.error_msg = req.flash("error_msg");
+    res.locals.error = req.flash("error");
+    next();
+});
+
+
+//routes
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`server started on port ${PORT}`));
---
+++ config/keys.js
@@ -0,0 +1,8 @@
+dbPassword =
+  "mongodb+srv://seth:seth1234@cluster0.7qzcw.mongodb.net/login2?retryWrites=true&w=majority";
+ 
+module.exports = {
+    mongoURI: dbPassword,
+};
+
+
